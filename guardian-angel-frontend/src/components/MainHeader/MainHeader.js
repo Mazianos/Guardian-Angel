deaecb7ea558383d0e4f559fbcacf39a71fcb77b
partrima <iknubs@gmail.com>
2021-03-13 14:18:49 -0800

    header responsive, env file created

--- guardian-angel-frontend/src/components/MainHeader/MainHeader.js
+++ guardian-angel-frontend/src/components/MainHeader/MainHeader.js
@@ -3,17 +3,33 @@ import { Box, Text, Image, Button, Avatar, Grommet } from 'grommet';
 import MainHeaderButtons from './MainHeaderButtons'
 
 class MainHeader extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            width: 0,
+            height: 0
+        }
+        window.addEventListener("resize", this.update);
+    }
+    componentDidMount() {
+        this.update();
+        console.log(process.env);
+    }
+    update = () => {
+        this.setState({
+            height: window.innerHeight,
+            width: window.innerWidth
+        });
+    };
+
+
     render() {
         return (
             <Box background="#7D4CDB" height='xxsmall' pad='xsmall'>
-                <Box direction='row' gap='small' basis="1/5">
+                <Box direction='row' gap='small' margin={{ vertical: 'auto' }}>
                     <Avatar round='auto' src="../../../GAlogo.png" size="small" margin={{ vertical: 'auto' }}></Avatar>
                     <Text size="medium" weight="bold" margin={{ vertical: 'auto' }}>Gaurdian Angle</Text>
-
-                    <Box style={{ position: 'absolute', right: '10px' }}>
-
-                        <MainHeaderButtons />
-                    </Box>
+                    <MainHeaderButtons width={this.state.width} />
                 </Box>
 
             </Box >
--- guardian-angel-frontend/src/components/MainHeader/MainHeaderButtons.js
+++ guardian-angel-frontend/src/components/MainHeader/MainHeaderButtons.js
@@ -1,31 +1,57 @@
 import React from "react";
-import { Box, Text, Image, Button, Avatar, Grommet } from 'grommet';
+import { Box, Text, Image, Button, Avatar, Grommet, Menu } from 'grommet';
+import { Apps } from 'grommet-icons';
 
-class MainHeaderButtons extends React.Component {
-    render() {
-        return (
-            <Box direction="row" gap='small'>
 
-                <Button
-                    primary
-                    size='small'
-                    color="#B27AFF"
-                    label="Sign up"
+function MainHeaderButtons({ width }) {
 
-                >
-                </Button>
-                <Button
-                    primary
-                    size='small'
-                    color="#B27AFF"
-                    label="Log in"
+    return (
+        <Box>
+            {width > 550 ?
+                <Box direction="row" gap='small' style={{ position: 'absolute', right: '10px', top: '8px' }}>
+                    <Button
+                        primary
+                        size='small'
+                        color="#B27AFF"
+                        label="Login / Signup"
 
-                >
-                </Button>
+                    >
+                    </Button>
+                    <Button
+                        primary
+                        size='small'
+                        color="#B27AFF"
+                        label="Register"
 
-            </Box>
-        )
-    }
+                    >
+                    </Button>
+                    <Button
+                        primary
+                        size='small'
+                        color="#B27AFF"
+                        label="Contact Us"
+
+                    >
+                    </Button>
+                </Box>
+                :
+                <Box direction="row" gap='small' style={{ position: 'absolute', right: '10px', top: '8px' }}>
+                    <Menu
+                        icon={<Apps></Apps>}
+                        color="red"
+                        items={[
+                            { label: 'Login / Signup', onClick: () => { } },
+                            { label: 'Register', onClick: () => { } },
+                            { label: 'Contact Us', onClick: () => { } },
+                        ]}
+
+                    >
+                    </Menu>
+                </Box>
+            }
+        </Box>
+
+    )
 }
 
 export default MainHeaderButtons;
