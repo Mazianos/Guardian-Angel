41ffad8850d6299d5b869a5826e70b141a73c5cb
partrima <iknubs@gmail.com>
2021-03-14 01:10:53 -0800

    added new animation package

--- guardian-angel-frontend/src/firebase.js
+++ guardian-angel-frontend/src/firebase.js
@@ -13,15 +13,17 @@ import "firebase/auth";
 // });
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyDMxJEBm62ckRWH5gBZkEyow-scvE9Qf4o",
-    authDomain: "guardian-angel-fd236.firebaseapp.com",
-    databaseURL: "https://guardian-angel-fd236-default-rtdb.firebaseio.com",
-    projectId: "guardian-angel-fd236",
-    storageBucket: "guardian-angel-fd236.appspot.com",
-    messagingSenderId: "174512453055",
-    appId: "1:174512453055:web:978404551696f221aa1d0a",
-    measurementId: "G-L923E24DQZ"
-  });
+  apiKey: "AIzaSyDMxJEBm62ckRWH5gBZkEyow-scvE9Qf4o",
+  authDomain: "guardian-angel-fd236.firebaseapp.com",
+  databaseURL: "https://guardian-angel-fd236-default-rtdb.firebaseio.com",
+  projectId: "guardian-angel-fd236",
+  storageBucket: "guardian-angel-fd236.appspot.com",
+  messagingSenderId: "174512453055",
+  appId: "1:174512453055:web:978404551696f221aa1d0a",
+  measurementId: "G-L923E24DQZ"
+});
+
 
 export default app;
