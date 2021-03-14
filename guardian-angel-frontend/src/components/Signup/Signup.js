78f8d98df8e54717fb3e82e773312637987b72be
partrima <iknubs@gmail.com>
2021-03-13 22:22:11 -0800

    fixed password positioning

--- guardian-angel-frontend/src/components/Signup/Signup.js
+++ guardian-angel-frontend/src/components/Signup/Signup.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { Box, Text, Heading, FormField, TextInput, Grommet, Button } from "grommet";
-import PasswordTextBox from './PasswordTextBox'
+import { Hide, View } from 'grommet-icons';
 
 function Login() {
     const [text, setText] = React.useState('');
+    const [value, setValue] = React.useState('');
+    const [reveal, setReveal] = React.useState(true);
 
     function logSignup() {
         console.log("Testing!");
     }
+
+    setTimeout(
+        () => setReveal(false),
+        200
+    );
+
 
     return (
         <Box background="#7D4CDB" style={{
@@ -51,14 +59,31 @@ function Login() {
 
 
                         <FormField label="Password">
-                            <PasswordTextBox emailFilled={text.length}></PasswordTextBox>
+                            <Box
+                                direction='row'
+                            >
+                                <TextInput
+                                    plain
+                                    style={{ borderRadius: "12px" }}
+                                    type={reveal ? 'text' : 'password'}
+                                    value={value}
+                                    onChange={event => setValue(event.target.value)}
+                                    placeholder="type here"
+                                    disabled={text.length == 0}
+
+                                />
+                                <Button
+                                    icon={reveal ? <View size="medium" color="white" /> : <Hide size="medium" color="white" />}
+                                    onClick={() => setReveal(!reveal)}
+                                />
+                            </Box>
                         </FormField>
                     </Grommet>
                 </Box>
             </Box>
             <Box width='small' margin="small" align="center">
 
-                <Button 
+                <Button
                     type="submit"
                     label="Create account"
                     secondary
