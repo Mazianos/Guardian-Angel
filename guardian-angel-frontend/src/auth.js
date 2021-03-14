0f5e9bb471c1ed87a5fc70dc77fd10e90f37520e
Mazianos <mazin.marwan@gmail.com>
2021-03-13 21:49:03 -0800

    Merging with main

--- guardian-angel-frontend/src/auth.js
+++ guardian-angel-frontend/src/auth.js
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from "react";
 import app from "./firebase.js";
 
-<<<<<<< HEAD
-export const AuthContext = React.createContext();
-
-export const AuthProvider = ({ children }) => {
-=======
 //Allows us to propogate this data across the entire tree of components
 export const AuthContext = React.createContext();
 
 //Holds the user and updates any time the authentication status changes
-export const AuthProvider = ({ childre }) => {
->>>>>>> 1fbdf143d516ecd0a04d0b21f31242acfd4d2b37
+export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
 
     useEffect(() => {
@@ -20,16 +14,6 @@ export const AuthProvider = ({ childre }) => {
 
     return (
         <AuthContext.Provider
-<<<<<<< HEAD
-        value={{
-            currentUser
-        }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
-};
-=======
         value = {{
             currentUser
         }}>
@@ -37,4 +21,3 @@ export const AuthProvider = ({ childre }) => {
         </AuthContext.Provider>
     )
 }
->>>>>>> 1fbdf143d516ecd0a04d0b21f31242acfd4d2b37
--- guardian-angel-frontend/src/firebase.js
+++ guardian-angel-frontend/src/firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
@@ -10,19 +9,6 @@ const app = firebase.initializeApp({
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
     measurementId: process.env.REACT_APP_measurementId
-=======
-import * as firebase from 'firebase';
-import "firebase/auth";
-
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderID: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appID: process.env.REACT_APP_FIREBASE_APPID,
-  measurementID: process.env.REACT_APP_FIREBASE_MEASUREMENTID
->>>>>>> 1fbdf143d516ecd0a04d0b21f31242acfd4d2b37
 });
 
 export default app;
